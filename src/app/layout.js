c281e5e733fcd82163827367dbfe8105f9b18c77
Unsigned0Varchar <123456789abhik@gmail.com>
2026-01-13 03:47:31 +0530

    More changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,14 @@ export default function RootLayout({ children }) {
       <body
         className={`${bricoloage.className} bg-rose-100 antialiased select-none`}
       >
+        <header className="fixed top-2 w-full text-center text-[10px] md:text-sm text-rose-500 font-medium z-50 px-4 opacity-80 pointer-events-none">
+          Software engineer hone ka kya hi matlab if i dont code a cute website a gift for my Bun... Hehe Love you
+        </header>
         {children}
+        <footer className="fixed bottom-2 w-full text-center text-sm text-rose-600 font-semibold pointer-events-none z-50 opacity-90">
+          Copyright 2026, Abhikraj
+        </footer>
+
       </body>
     </html>
   );
